2156a6a75849535bb220b52fbb4caadc2ff5bbd1
Kakitsan <56282204+Kakitsan@users.noreply.github.com>
2020-03-18 16:50:22 -0700

    updated

--- sketch.js
+++ sketch.js
@@ -1,20 +1,39 @@
-let array = [];
+// let array = [];
+// let backgroundColor = 200;
+let noiseoffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
-  createCanvas(600, 600);
-  background(255);
-  strokeWeight(5);
-  noFill();
+  createCanvas(windowWidth, windowHeight);
+  // background(backgroundColor);
+  background(220, 50, 133);
+
+
+  // noFill();
 }
 
 function draw() {
-  if (mouseIsPressed){
-    // stroke(map(mouseX, 0, 600, 255, true));
-    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    // line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
-    array.push([mouseX, mouseY]);
-  }
+  // if (mouseIsPressed){
+    strokeWeight(strokeWidth);
+    background(220, 50, 133, 5);
+
+    noiseoffset += 0.05;
+    strokeWidth = noise(noiseoffset) * 20;
+
+    stroke(map(mouseX, 0, 600, 255, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    // backgroundColor-= 4;
+    // background(backgroundColor);
+
+    // array.push([mouseX, mouseY]);
+    // beginShape();
+    // for(let i = 0; i<array.length; i++){
+    //   curveVertex(array[i][0], array[i][1]);
+    //
+    // }
+    // endShape();
+  // }
 }
 
 function keyTyped(){
@@ -22,16 +41,25 @@ function keyTyped(){
     saveCanvas('fileName', 'PNG');
   }
   else if(key === 'd'){
-    background(255);
-    beginShape();
-    for(let i = 0; i < array.length - 1; i++){
-      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array[i][1]);
-    }
-    endShape();
+
+    clear();
+    // background(255);
+    // beginShape();
+    // for(let i = 0; i < array.length - 1; i++){
+    //   // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    //   curveVertex(array[i][0], array[i][1]);
+    // }
+    // endShape();
 
     // line(array[1][0], array[1][1], array[2][0], array[2][1]);
 
 }
   return false;
 }
+
+  // function mouseIsPressed(){
+  //   array = [];
+  //   backgroundColor = 255;
+  //
+  //
+  // }
